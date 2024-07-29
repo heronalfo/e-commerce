98dcf98ec6dc3e1829618c52ed732012c3550f00
heronalfo <j040p3dr0s1lv4s4nt0s@gmail.com>
2024-07-29 18:25:19 -0300

    Created a new feature for creating carts.

--- content/env-example.py
+++ content/env-example.py
@@ -5,6 +5,7 @@ This module is responsible for defining universal variables for the
 entire Django project and configuration.
 '''
 
+
 SECRET_KEY = 'django-insecure-s44o_bc@y)n$wkqbigp4(z-dmndb7nj)wm(!!bzr2$1he$^k#@'
 
 #Important: If you use a specific SGBD, first install the client for this engine
